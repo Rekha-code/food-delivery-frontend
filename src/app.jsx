e3b7eca3b0cc0a362aede7a841b27a336d079391
rekha_gorde <rekha.gorde8@gmail.com>
2024-03-28 13:09:24 +0530

    food-delivery-frontend

---
+++ src/app.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./app.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
+
+export function App() {
+  const [showLogin, setShowLogin] = useState(false);
+  return (
+    <>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+}
